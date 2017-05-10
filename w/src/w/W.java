95a408f3cb98f1820f0df9175906ab7c13f94eb8
Mirotus Solekhah <16523161@students.uii.ac.id>
2017-05-10 16:18:45 +0800

    merubah

--- w/src/w/W.java
+++ w/src/w/W.java
@@ -17,7 +17,7 @@ public class W {
      */
     public static void main(String[] args) {
         System.out.println("wadadaw");
-        System.out.println("cek");
+        System.out.print("cek : ");
         System.out.println("Selamat datang Kelas D RPL FTI UII");
     }
     
